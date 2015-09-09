6380a0e2173c7e6192faa02920b820eabaf11835
dkhwangbo <dkhwangbo@gruter.com>
2015-09-09 17:59:27 +0900

    improve

--- src/main/java/TajoBenchmark.java
+++ src/main/java/TajoBenchmark.java
@@ -1,53 +1,105 @@
-
 import org.apache.tajo.client.v2.TajoClient;
 import org.apache.tajo.client.v2.exception.ClientUnableToConnectException;
 import org.apache.tajo.exception.QueryFailedException;
 import org.apache.tajo.exception.QueryKilledException;
 import org.apache.tajo.exception.TajoException;
 
+import java.io.*;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.List;
 
 public class TajoBenchmark {
 
-    public static void run(String hostname, int port, String dataBase, String sql) throws ClientUnableToConnectException {
+    public void run(String hostname, int port, String dataBase, String fileName, String sql) throws ClientUnableToConnectException {
+        System.out.print(fileName + " ---------- ");
+
         try (TajoClient client = new TajoClient(hostname, port)) {
             try {
-                int total = 1;
-                int pre = 0, cur =0;
                 client.selectDB(dataBase);
-                ResultSet result = client.executeQuery(sql);
-                result.next();
+                List<String> query = Arrays.asList(sql.split(";"));
 
-                while(result.next()) {
-                    total++;
-                    cur = result.getInt("l_orderkey");
+                Iterator<String> i = query.iterator();
+                int count = 0;
 
-                    if(pre-cur < 0)
-                        System.out.println("ERROR in " + total + " row! PRE = " + pre + ", CUR = " + cur);
-
-                    pre = cur;
+                long startTime = System.currentTimeMillis();
+                while(true) {
+                    count++;
+                    client.executeQuery(i.next() + ";");
+                    if(count == query.size()-1) {
+                        break;
+                    }
                 }
 
-                System.out.println("Total row : " + Integer.toString(total));
-
+                long duration = System.currentTimeMillis() - startTime;
+                NumberFormat formatter = new DecimalFormat("#0.00000");
+                System.out.print("QUERY SUCCESS! Execution time : " + formatter.format(duration / 1000d) + " seconds");
             } catch (QueryFailedException e) {
-                System.err.println("query is failed.");
+                System.err.print("FAILED.");
             } catch (QueryKilledException e) {
-                System.err.println("query is killed.");
-            } catch (SQLException | TajoException e) {
+                System.err.print("KILLED.");
+            } catch (Exception e) {
+                System.err.println("OTHER EXCEPTION.");
                 e.printStackTrace();
+            } finally {
+                client.close();
             }
-
         }
+
+        System.out.println();
     }
 
-    public static void main(String[] args) throws ClientUnableToConnectException {
-        String hostname = "localhost";
-        int port = 26002;
-        String dataBase = "tpch";
-        String sql = "SELECT * FROM LINEITEM ORDER BY L_ORDERKEY DESC;";
+    public int Driver() throws ClientUnableToConnectException, IOException {
+        Scanner s = new Scanner(System.in);
 
-        run(hostname, port, dataBase, sql);
+        System.out.print("hostname : ");
+//        String hostname = "localhost";
+        String hostname = s.nextLine();
+        System.out.print("port : ");
+        int port = s.nextInt();
+//        int port = 26002;
+        System.out.print("database : ");
+//        String dataBase = "tpch";
+        String dataBase = s.nextLine();
+
+        System.out.print("query directory : ");
+
+//        File folder = new File(s.nextLine());
+        File folder = new File("/home/dk/Benchmarking/tajo/tpc-h/query_revised");
+        File[] listOfFiles = folder.listFiles();
+        Arrays.sort(listOfFiles);
+        System.out.println("\nSTARTING! total : " + listOfFiles.length);
+        for (File file : listOfFiles) {
+            if (file.isFile()) {
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                try {
+                    StringBuilder sb = new StringBuilder();
+                    String line = br.readLine();
+
+                    while (line != null) {
+                        sb.append(line);
+                        sb.append(" ");
+                        line = br.readLine();
+                    }
+
+                    run(hostname, port, dataBase, file.getName(), sb.toString());
+                } finally {
+                    br.close();
+                }
+            }
+        }
+        System.out.println("ALL TEST IS DONE");
+
+        return 0;
+    }
+
+    public static void main(String args[]) throws ClientUnableToConnectException, IOException {
+        TajoBenchmark t = new TajoBenchmark();
+        System.exit(t.Driver());
     }
 }
